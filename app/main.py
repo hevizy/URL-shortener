dcd3b05c09804f1755adb1904ec548257d995d34
hevizy <hevizy134@gmail.com>
2026-09-12 13:11:24 +0300

    refactor: update test endpoint

--- app/main.py
+++ app/main.py
@@ -5,6 +5,6 @@ from app.routes import url_router
 app = FastAPI()
 
 app.include_router(url_router)
-@app.get("/main/root")
+@app.get("/root/")
 async def root():
     return {"message": "Hello World"}
